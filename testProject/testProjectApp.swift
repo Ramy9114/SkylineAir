2708d13df324b2d0d817231e8e5692f959b65700
Rami Moubayed <rmoubayed12@gmail.com>
2026-03-16 21:45:36 +0100

    Initial Commit

---
+++ testProject/testProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  testProjectApp.swift
+//  testProject
+//
+//  Created by Rami on 16/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct testProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
